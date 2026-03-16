fe14ae0a8bbe5ad72e90b38a98df53fd6f3633e8
Paweł Niekłań <p.nieklan@donedeliveries.com>
2026-03-16 20:09:58 +0100

    Upgrade to HERE Routing API v8
    - Rewrite router to use HERE API v8 with apiKey authentication
    - Add flexible-polyline decoder inline
    - Update example with API key form and session storage
    - Add .gitignore and remove node_modules from tracking
    - Clean up unused files

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,93 @@
-var map = L.map('map');
+(function () {
+	var STORAGE_KEY = 'here_api_key';
+	var map, routingControl;
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+	// Initialize map
+	function initMap() {
+		map = L.map('map').setView([52.0, 19.0], 6);
 
-L.Routing.control({
-	waypoints: [
-		L.latLng(57.74, 11.94),
-		L.latLng(57.6792, 11.949)
-	],
-	geocoder: L.Control.Geocoder.nominatim(),
-	router: L.Routing.here('here app id', 'here app code'),
-	routeWhileDragging: false
-}).addTo(map);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+	}
+
+	// Initialize routing with API key
+	function initRouting(apiKey) {
+		if (routingControl) {
+			map.removeControl(routingControl);
+		}
+
+		routingControl = L.Routing.control({
+			waypoints: [
+				L.latLng(52.2297, 21.0122), // Warszawa
+				L.latLng(50.0647, 19.9450)  // Kraków
+			],
+			router: L.Routing.here({
+				apiKey: apiKey
+			}),
+			routeWhileDragging: true
+		}).addTo(map);
+
+		// Add control to show/change API key
+		addApiKeyControl(apiKey);
+	}
+
+	// Add small control to show current key status
+	function addApiKeyControl(apiKey) {
+		var control = L.control({ position: 'bottomleft' });
+		control.onAdd = function () {
+			var div = L.DomUtil.create('div', 'api-key-control');
+			var maskedKey = apiKey.substring(0, 8) + '...' + apiKey.substring(apiKey.length - 4);
+			div.innerHTML = 'API Key: ' + maskedKey + '<button id="clear-key">Wyczysc</button>';
+			L.DomEvent.disableClickPropagation(div);
+			return div;
+		};
+		control.addTo(map);
+
+		// Handle clear button
+		setTimeout(function () {
+			var clearBtn = document.getElementById('clear-key');
+			if (clearBtn) {
+				clearBtn.addEventListener('click', function () {
+					sessionStorage.removeItem(STORAGE_KEY);
+					location.reload();
+				});
+			}
+		}, 0);
+	}
+
+	// Show API key form
+	function showForm() {
+		var form = document.getElementById('api-key-form');
+		var input = document.getElementById('api-key-input');
+		var button = document.getElementById('api-key-submit');
+
+		form.classList.remove('hidden');
+		input.focus();
+
+		function submit() {
+			var apiKey = input.value.trim();
+			if (apiKey) {
+				sessionStorage.setItem(STORAGE_KEY, apiKey);
+				form.classList.add('hidden');
+				initRouting(apiKey);
+			}
+		}
+
+		button.addEventListener('click', submit);
+		input.addEventListener('keypress', function (e) {
+			if (e.key === 'Enter') submit();
+		});
+	}
+
+	// Main
+	initMap();
+
+	var savedKey = sessionStorage.getItem(STORAGE_KEY);
+	if (savedKey) {
+		document.getElementById('api-key-form').classList.add('hidden');
+		initRouting(savedKey);
+	} else {
+		showForm();
+	}
+})();
--- src/L.Routing.Here.js
+++ src/L.Routing.Here.js
@@ -1,138 +1,267 @@
 (function () {
 	'use strict';
 
-	var L = require('leaflet');
-	var corslite = require('corslite');
-	var haversine = require('haversine');
+	// Flexible Polyline decoder (HERE format)
+	var flexiblePolyline = (function () {
+		var DECODING_TABLE = [
+			62, -1, -1, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1,
+			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+			22, 23, 24, 25, -1, -1, -1, -1, 63, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+			36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
+		];
+
+		function decodeChar(char) {
+			return DECODING_TABLE[char.charCodeAt(0) - 45];
+		}
+
+		function decodeUnsignedValues(encoded) {
+			var result = 0;
+			var shift = 0;
+			var resList = [];
+
+			for (var i = 0; i < encoded.length; i++) {
+				var value = decodeChar(encoded[i]);
+				result |= (value & 0x1F) << shift;
+				if ((value & 0x20) === 0) {
+					resList.push(result);
+					result = 0;
+					shift = 0;
+				} else {
+					shift += 5;
+				}
+			}
+			return resList;
+		}
+
+		function toSigned(val) {
+			var res = val;
+			if (res & 1) {
+				res = ~res;
+			}
+			res >>= 1;
+			return res;
+		}
+
+		return {
+			decode: function (encoded) {
+				var values = decodeUnsignedValues(encoded);
+				var header = values[1];
+				var precision = header & 15;
+				var thirdDim = (header >> 4) & 7;
+				var thirdDimPrecision = (header >> 7) & 15;
+
+				var factorDegree = Math.pow(10, precision);
+				var factorZ = Math.pow(10, thirdDimPrecision);
+
+				var lastLat = 0, lastLng = 0, lastZ = 0;
+				var res = [];
+				var i = 2;
+
+				while (i < values.length) {
+					lastLat += toSigned(values[i]) / factorDegree;
+					lastLng += toSigned(values[i + 1]) / factorDegree;
+
+					if (thirdDim) {
+						lastZ += toSigned(values[i + 2]) / factorZ;
+						res.push([lastLat, lastLng, lastZ]);
+						i += 3;
+					} else {
+						res.push([lastLat, lastLng]);
+						i += 2;
+					}
+				}
+
+				return { polyline: res };
+			}
+		};
+	})();
+
+	// Get Leaflet from global scope
+	var L = window.L;
+	if (!L) {
+		throw new Error('Leaflet must be loaded before lrm-here');
+	}
 
 	L.Routing = L.Routing || {};
 
+	function is(className, object) {
+		return Object.prototype.toString.call(object) === '[object ' + className + ']';
+	}
+
+	// Encoder for nested URL parameters (e.g. avoid[features][0]=tollRoad)
+	var DataEncoder = function () {
+		this.levels = [];
+		this.actualKey = null;
+	};
+
+	DataEncoder.prototype._encode = function (data) {
+		var uriPart = '';
+		var levelsSize = this.levels.length;
+		if (levelsSize) {
+			uriPart = this.levels[0];
+			for (var c = 1; c < levelsSize; c++) {
+				uriPart += '[' + this.levels[c] + ']';
+			}
+		}
+		var finalString = '';
+		if (is('Object', data)) {
+			var keys = Object.keys(data);
+			for (var a = 0; a < keys.length; a++) {
+				var key = keys[a];
+				this.actualKey = key;
+				this.levels.push(this.actualKey);
+				finalString += this._encode(data[key]);
+			}
+		} else if (is('Array', data)) {
+			if (!this.actualKey) throw new Error('Directly passed array does not work');
+			for (var b = 0; b < data.length; b++) {
+				this.levels.push(b);
+				finalString += this._encode(data[b]);
+			}
+		} else {
+			finalString += uriPart + '=' + encodeURIComponent(data) + '&';
+		}
+		this.levels.pop();
+		return finalString;
+	};
+
+	DataEncoder.prototype.encode = function (data) {
+		if (!is('Object', data) || Object.keys(data).length === 0) return '';
+		var encoded = this._encode(data);
+		return encoded ? '&' + encoded.slice(0, -1) : '';
+	};
+
 	L.Routing.Here = L.Class.extend({
 		options: {
-			serviceUrl: 'https://route.api.here.com/routing/7.2/calculateroute.json',
+			serviceUrl: 'https://router.hereapi.com/v8/routes',
+			apiKey: '',
 			timeout: 30 * 1000,
 			alternatives: 0,
-			mode: 'fastest;car;',
-			generateMode: false,
-			urlParameters: {}
+			routingMode: 'fast', // fast, short
+			transportMode: 'car', // car, truck, pedestrian, bicycle, scooter
+			return: ['polyline', 'instructions', 'actions', 'summary'],
+			avoid: {
+				features: [] // tollRoad, controlledAccessHighway, ferry, tunnel, dirtRoad
+			},
+			truck: {} // height, width, length, weight, axleCount, etc.
 		},
 
-		initialize: function (appId, appCode, options) {
-			this._appId = appId;
-			this._appCode = appCode;
+		initialize: function (options) {
 			L.Util.setOptions(this, options);
+			if (!this.options.apiKey) {
+				throw new Error('HERE API key is required');
+			}
 		},
 
 		route: function (waypoints, callback, context, options) {
-			var timedOut = false,
-				wps = [],
-				url,
-				timer,
-				wp,
-				i;
+			var timedOut = false;
+			var self = this;
 
 			options = options || {};
-			url = this.buildRouteUrl(waypoints, options);
+			var url = this.buildRouteUrl(waypoints, options);
 
-			timer = setTimeout(function () {
+			var timer = setTimeout(function () {
 				timedOut = true;
 				callback.call(context || callback, {
 					status: -1,
-					message: 'Here request timed out.'
+					message: 'HERE request timed out.'
 				});
 			}, this.options.timeout);
 
-			// for (i = 0; i < waypoints.length; i++) {
-			// 	wp = waypoints[i];
-			// 	wps.push({
-			// 		latLng: wp.latLng,
-			// 		name: wp.name,
-			// 		options: wp.options
-			// 	});
-			// }
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', url, true);
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState === 4) {
+					clearTimeout(timer);
+					if (timedOut) return;
 
-			// Let reference here, problem when reverse geocoding point took to long, didnt have name here
-			wps = waypoints;
-
-			return corslite(url, L.bind(function (err, resp) {
-				var data;
-
-				clearTimeout(timer);
-				if (!timedOut) {
-					if (!err) {
-						data = JSON.parse(resp.responseText);
-						this._routeDone(data, wps, callback, context);
+					if (xhr.status >= 200 && xhr.status < 300) {
+						var data = JSON.parse(xhr.responseText);
+						self._routeDone(data, waypoints, callback, context);
 					} else {
+						var error = xhr.responseText ? JSON.parse(xhr.responseText) : {};
 						callback.call(context || callback, {
-							status: -1,
-							message: 'HTTP request failed: ' + err,
-							type: err.type
+							status: xhr.status,
+							message: error.title || 'HTTP request failed',
+							details: error
 						});
 					}
 				}
-			}, this));
+			};
+			xhr.onerror = function () {
+				clearTimeout(timer);
+				if (!timedOut) {
+					callback.call(context || callback, {
+						status: -1,
+						message: 'HTTP request failed'
+					});
+				}
+			};
+			xhr.send();
+
+			return xhr;
 		},
 
 		_routeDone: function (response, inputWaypoints, callback, context) {
-			var alts = [],
-				waypoints,
-				waypoint,
-				coordinates,
-				i, j, k,
-				instructions,
-				distance,
-				time,
-				leg,
-				maneuver,
-				startingSearchIndex,
-				instruction,
-				path;
-
+			var alts = [];
 			context = context || callback;
-			if (!response.response.route) {
+
+			if (!response.routes || response.routes.length === 0) {
 				callback.call(context, {
-					// TODO: include all errors
-					status: response.type,
-					message: response.details
+					status: response.status || 'NO_ROUTE',
+					message: response.title || 'No route found'
 				});
 				return;
 			}
 
-			for (i = 0; i < response.response.route.length; i++) {
-				path = response.response.route[i];
-				coordinates = this._decodeGeometry(path.shape);
-				startingSearchIndex = 0;
+			for (var i = 0; i < response.routes.length; i++) {
+				var route = response.routes[i];
+				var coordinates = [];
+				var instructions = [];
+				var totalDistance = 0;
+				var totalTime = 0;
+				var startingSearchIndex = 0;
 
-				instructions = [];
-				time = 0;
-				distance = 0;
-				for (j = 0; j < path.leg.length; j++) {
-					leg = path.leg[j];
-					for (k = 0; k < leg.maneuver.length; k++) {
-						maneuver = leg.maneuver[k];
-						distance += maneuver.length;
-						time += maneuver.travelTime;
-						instruction = this._convertInstruction(maneuver, coordinates, startingSearchIndex);
-						instructions.push(instruction);
-						startingSearchIndex = instruction.index;
+				for (var j = 0; j < route.sections.length; j++) {
+					var section = route.sections[j];
+
+					// Decode polyline
+					var decoded = flexiblePolyline.decode(section.polyline);
+					coordinates = coordinates.concat(decoded.polyline);
+
+					// Summary
+					if (section.summary) {
+						totalDistance += section.summary.length || 0;
+						totalTime += section.summary.duration || 0;
+					}
+
+					// Instructions from actions
+					if (section.actions) {
+						for (var k = 0; k < section.actions.length; k++) {
+							var action = section.actions[k];
+							var instruction = this._convertInstruction(action, coordinates, startingSearchIndex);
+							instructions.push(instruction);
+							startingSearchIndex = instruction.index;
+						}
 					}
 				}
 
-				waypoints = [];
-				for (j = 0; j < path.waypoint.length; j++) {
-					waypoint = path.waypoint[j];
+				// Build waypoints from input
+				var waypoints = [];
+				for (var w = 0; w < inputWaypoints.length; w++) {
 					waypoints.push(new L.LatLng(
-						waypoint.mappedPosition.latitude,
-						waypoint.mappedPosition.longitude));
+						inputWaypoints[w].latLng.lat,
+						inputWaypoints[w].latLng.lng
+					));
 				}
 
 				alts.push({
-					name: path.label.join(', '),
+					name: this._buildRouteName(route),
 					coordinates: coordinates,
 					instructions: instructions,
 					summary: {
-						totalDistance: distance,
-						totalTime: time,
+						totalDistance: totalDistance,
+						totalTime: totalTime
 					},
 					inputWaypoints: inputWaypoints,
 					waypoints: waypoints
@@ -142,162 +271,120 @@
 			callback.call(context, null, alts);
 		},
 
-		_decodeGeometry: function (geometry) {
-			var latlngs = new Array(geometry.length),
-				coord,
-				i;
-			for (i = 0; i < geometry.length; i++) {
-				coord = geometry[i].split(',');
-				latlngs[i] = ([parseFloat(coord[0]), parseFloat(coord[1])]);
+		_buildRouteName: function (route) {
+			var names = [];
+			if (route.sections) {
+				for (var i = 0; i < route.sections.length; i++) {
+					var section = route.sections[i];
+					if (section.summary && section.summary.text) {
+						names.push(section.summary.text);
+					}
+				}
 			}
-
-			return latlngs;
+			return names.join(', ') || '';
 		},
 
 		buildRouteUrl: function (waypoints, options) {
-			var locs = [],
-				i,
-				alternatives,
-				baseUrl;
+			var params = [];
 
-			for (i = 0; i < waypoints.length; i++) {
-				locs.push('waypoint' + i + '=geo!' + waypoints[i].latLng.lat + ',' + waypoints[i].latLng.lng);
+			// Origin
+			params.push('origin=' + waypoints[0].latLng.lat + ',' + waypoints[0].latLng.lng);
+
+			// Via points
+			for (var i = 1; i < waypoints.length - 1; i++) {
+				params.push('via=' + waypoints[i].latLng.lat + ',' + waypoints[i].latLng.lng);
 			}
 
-			alternatives = this.options.alternatives;
-			baseUrl = this.options.serviceUrl + '?' + locs.join('&');
+			// Destination
+			params.push('destination=' + waypoints[waypoints.length - 1].latLng.lat + ',' + waypoints[waypoints.length - 1].latLng.lng);
 
-			return baseUrl + L.Util.getParamString(L.extend({
-				instructionFormat: 'text',
-				app_code: this._appCode,
-				app_id: this._appId,
-				representation: 'navigation',
-				mode: this._buildRouteMode(this.options),
-				alternatives: alternatives
-			}, this.options.urlParameters, this._attachTruckRestrictions(this.options)), baseUrl);
+			// Required params
+			params.push('apiKey=' + encodeURIComponent(this.options.apiKey));
+			params.push('transportMode=' + this.options.transportMode);
+			params.push('routingMode=' + this.options.routingMode);
+			params.push('return=' + this.options.return.join(','));
+
+			// Alternatives
+			if (this.options.alternatives > 0) {
+				params.push('alternatives=' + this.options.alternatives);
+			}
+
+			// Avoid features
+			var encoder = new DataEncoder();
+			var avoidParams = this._buildAvoidParams();
+			if (Object.keys(avoidParams).length > 0) {
+				var avoidEncoded = encoder.encode({ avoid: avoidParams });
+				if (avoidEncoded) {
+					params.push(avoidEncoded.substring(1));
+				}
+			}
+
+			// Truck parameters
+			if (this.options.transportMode === 'truck') {
+				var truckParams = this._buildTruckParams();
+				var truckEncoded = encoder.encode({ truck: truckParams });
+				if (truckEncoded) {
+					params.push(truckEncoded.substring(1));
+				}
+			}
+
+			return this.options.serviceUrl + '?' + params.join('&');
 		},
 
-		_buildRouteMode: function (options) {
-			if (options.generateMode === false) {
-				return options.mode;
-			}
-			var modes = [];
-			var avoidness = [];
-			var avoidnessLevel = '-3'; //strictExclude
+		_buildAvoidParams: function () {
+			var avoid = {};
+			var features = this.options.avoid.features;
 
-			if (options.hasOwnProperty('routeRestriction')
-				&& options.routeRestriction.hasOwnProperty('routeType')) {
-				modes.push(options.routeRestriction.routeType);
+			if (features && features.length > 0) {
+				avoid.features = features;
 			}
-			else {
-				modes.push('fastest');
+			if (this.options.avoid.areas) {
+				avoid.areas = this.options.avoid.areas;
 			}
 
-			if (options.hasOwnProperty('routeRestriction')
-				&& options.routeRestriction.hasOwnProperty('vehicleType')) {
-				modes.push(options.routeRestriction.vehicleType);
-			} else {
-				modes.push('car');
-			}
-
-			if (options.hasOwnProperty('routeRestriction')
-				&& options.routeRestriction.hasOwnProperty('trafficMode')
-				&& options.routeRestriction.trafficMode === true) {
-				modes.push('traffic:enabled');
-			} else {
-				modes.push('traffic:disabled');
-			}
-
-			if (options.hasOwnProperty('routeRestriction')
-				&& options.routeRestriction.hasOwnProperty('avoidHighways')
-				&& options.routeRestriction.avoidHighways === true) {
-				avoidness.push('motorway:' + avoidnessLevel);
-			}
-
-			if (options.hasOwnProperty('routeRestriction')
-				&& options.routeRestriction.hasOwnProperty('avoidTolls')
-				&& options.routeRestriction.avoidTolls === true) {
-				avoidness.push('tollroad:' + avoidnessLevel);
-			}
-
-			if (options.hasOwnProperty('routeRestriction')
-				&& options.routeRestriction.hasOwnProperty('avoidFerries')
-				&& options.routeRestriction.avoidFerries === true) {
-				avoidness.push('boatFerry:' + avoidnessLevel);
-			}
-
-			modes.push(avoidness.join(','));
-			return modes.join(';');
+			return avoid;
 		},
 
-		_attachTruckRestrictions: function (options) {
-			var _truckRestrictions = {};
-			var allowedParameters = ['height', 'width', 'length', 'limitedWeight', 'weightPerAxle', 'shippedHazardousGoods', 'engineType', 'trailersCount'];
+		_buildTruckParams: function () {
+			var truck = this.options.truck || {};
+			var params = {};
+			var allowedParams = [
+				'height', 'width', 'length', 'grossWeight', 'weightPerAxle',
+				'axleCount', 'trailerCount', 'type', 'shippedHazardousGoods'
+			];
 
-			if (!options.hasOwnProperty('routeRestriction')
-				|| !options.hasOwnProperty('truckRestriction')
-				|| !options.routeRestriction.hasOwnProperty('vehicleType')
-				|| options.routeRestriction.vehicleType !== 'truck') {
-				return _truckRestrictions;
-			}
-
-			if (options.truckRestriction.hasOwnProperty('shippedHazardousGoods')) {
-				if (Array.isArray(options.truckRestriction['shippedHazardousGoods'])) {
-					options.truckRestriction['shippedHazardousGoods'] = options.truckRestriction['shippedHazardousGoods'].join();
+			for (var key in truck) {
+				if (truck.hasOwnProperty(key) && allowedParams.indexOf(key) !== -1) {
+					params[key] = truck[key];
 				}
 			}
 
-			for (var property in options.truckRestriction) {
-				if (!options.truckRestriction.hasOwnProperty(property)
-					|| allowedParameters.indexOf(property) === -1
-					|| options.truckRestriction[property] === ''
-					|| options.truckRestriction[property] === null) {
-					continue;
-				}
-
-				var _value = options.truckRestriction[property];
-
-				if (property === 'engineType') {
-					property = 'vehicleType';
-				}
-				_truckRestrictions[property] = _value;
-			}
-			_truckRestrictions.truckType = 'truck';
-
-
-			return _truckRestrictions;
+			return params;
 		},
 
-		_convertInstruction: function (instruction, coordinates, startingSearchIndex) {
-			var i,
-				distance,
-				closestDistance = 0,
-				closestIndex = -1,
-				coordinate = instruction.position;
-			if (startingSearchIndex < 0) {
-				startingSearchIndex = 0;
+		_convertInstruction: function (action, coordinates, startingSearchIndex) {
+			var closestIndex = startingSearchIndex;
+
+			if (typeof action.offset === 'number' && action.offset < coordinates.length) {
+				closestIndex = action.offset;
+			} else if (startingSearchIndex < coordinates.length) {
+				closestIndex = startingSearchIndex;
 			}
-			for (i = startingSearchIndex; i < coordinates.length; i++) {
-				distance = haversine(coordinate, { latitude: coordinates[i][0], longitude: coordinates[i][1] });
-				if (distance < closestDistance || closestIndex == -1) {
-					closestDistance = distance;
-					closestIndex = i;
-				}
-			}
+
 			return {
-				text: instruction.instruction,//text,
-				distance: instruction.length,
-				time: instruction.travelTime,
+				text: action.instruction || '',
+				distance: action.length || 0,
+				time: action.duration || 0,
 				index: closestIndex,
-				type: instruction.action,
-				road: instruction.roadName
+				type: action.action || '',
+				direction: action.direction || '',
+				road: action.nextRoad ? action.nextRoad.name : ''
 			};
 		}
 	});
 
-	L.Routing.here = function (appId, appCode, options) {
-		return new L.Routing.Here(appId, appCode, options);
+	L.Routing.here = function (options) {
+		return new L.Routing.Here(options);
 	};
 
-	module.exports = L.Routing.Here;
 })();
